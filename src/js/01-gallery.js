62c71d66a014ea7df232e41181ae8e34e3fff8a2
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-11-07 17:06:41 +0200

    виконав 1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,47 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const bodyEl= document.querySelector('body')
+const galleryList =document.querySelector('.gallery')
+const markupEl=markupElemGallery(galleryItems)
+
+galleryList.insertAdjacentHTML('beforeend',markupEl)
+
+function markupElemGallery(galleryItems){
+    
+    return galleryItems.map(({preview,original,description})=>{
+    return`
+    <div class="gallery">
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                title="${description}"
+            />
+        </a>
+    </div>`;   
+}).join('');
+}
+
+
+
+galleryList.addEventListener('click',onDivGalleryClick)
+let gallery = new SimpleLightbox('.gallery a');
+console.log(gallery)
+
+function onDivGalleryClick(event){
+    event.preventDefault()
+
+
+   if(event.target.nodeName!=='IMG'){
+    return
+   }
+   gallery.options.captionDelay=250;
+   gallery.options.animationSpeed=350;
+   
+}
